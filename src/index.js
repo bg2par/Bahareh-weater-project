b9cde38f94be3b47c364d49ceb8ef0ce6cb333a5
bg2par <98562425+bg2par@users.noreply.github.com>
2024-01-19 22:54:34 +0330

    create searchCity function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-
+function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then();
+  }
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-
+    searchCity(searchInput.value);
   }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
