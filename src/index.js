ade26277431e69b5513357699537969d50af2b97
bg2par <98562425+bg2par@users.noreply.github.com>
2024-01-19 22:56:43 +0330

    create refreshWeather function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+function refreshWeather(response) {
+
+}
 function searchCity(city) {
     let apiKey = "b2a5adcct04b33178913oc335f405433";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then();
+    axios.get(apiUrl).then(refreshWeather);
   }
 function handleSearchSubmit(event) {
     event.preventDefault();
