056236ef065d19c03a9a43f2935674688fbf1463
bg2par <98562425+bg2par@users.noreply.github.com>
2024-01-23 01:10:31 +0330

    Using the API in the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,63 @@
-function refreshWeather(response) {
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+  function displayForecast(response) {
+    let forecastHtml = "";
+  
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml+=
+          `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div>
+          </div>
+        </div>
+      `;
+      }
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
+  function getForecast(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+  function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
@@ -35,65 +94,19 @@ function refreshWeather(response) {
     else if(response.data.condition.description == "scattered clouds" || "broken clouds" || "overcast clouds" || "few clouds"){
       backgroundImage.style.backgroundImage =  "url('src/images/clouds.jpg')";
     }
+    getForecast(response.data.city);
   }
-  
-  function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-  
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    return `${day} ${hours}:${minutes}`;
-  }
-  
   function searchCity(city) {
     let apiKey = "b2a5adcct04b33178913oc335f405433";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
   }
-  
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
   
     searchCity(searchInput.value);
-  }
-  function displayForecast() {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-    let forecastHtml = "";
-  
-    days.forEach(function (day) {
-      forecastHtml =
-        forecastHtml +
-        `
-        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-              <strong>15º</strong>
-            </div>
-            <div class="weather-forecast-temperature">9º</div>
-          </div>
-        </div>
-      `;
-    });
-  
-    let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = forecastHtml;
-  }
+  } 
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
